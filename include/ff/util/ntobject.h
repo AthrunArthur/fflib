273032f55549fe8bc5bf91e18caa4cc80ecc2076
Xuepeng.Fan <athrunarthur@gmail.com>
2020-04-19 19:30:00 +0800

    support c++11 instead of c++14

--- include/ff/util/ntobject.h
+++ include/ff/util/ntobject.h
@@ -181,16 +181,18 @@ private:
     // TODO optimize for rvalue(&&)
     template <typename T1, typename T2>
     static auto copy(T1 &target, T2 &&source) -> typename std::enable_if<
-        (std::remove_reference_t<T2>::type_list::len > Index), void>::type {
+        (std::remove_reference<T2>::type::type_list::len > Index), void>::type {
       typedef typename get_type_at_index_in_typelist<
-          typename std::remove_reference_t<T2>::type_list, Index>::type c_type;
+          typename std::remove_reference<T2>::type::type_list, Index>::type
+          c_type;
       target.template set<c_type>(source.template get<c_type>());
       copy_helper<Index + 1>::copy(target, std::forward<T2>(source));
     }
 
     template <typename T1, typename T2>
     static auto copy(T1 &target, const T2 &source) -> typename std::enable_if<
-        (std::remove_reference_t<T2>::type_list::len <= Index), void>::type {}
+        (std::remove_reference<T2>::type::type_list::len <= Index),
+        void>::type {}
   };
 };
 
