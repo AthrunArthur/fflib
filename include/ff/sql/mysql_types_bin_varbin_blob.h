835ce3d9e99674b3a21d7323b3d31705957dbb39
zwang <zwang@sgx-server>
2022-03-17 14:58:00 +0800

    support binarys varbin blob

--- include/ff/sql/mysql_types_bin_varbin_blob.h
+++ include/ff/sql/mysql_types_bin_varbin_blob.h
@@ -27,24 +27,23 @@
 
 
 
-// for char
+// for Binary
 namespace ff {
 namespace mysql {
-template <uint8_t Len> struct char_m{
+template <uint8_t Len> struct bin_m{
 public:
-  char_m() : m_data(){};
-  char_m(const char *s) : m_data(s){};
-  char_m(const ::sql::SQLString &s) : m_data(s.c_str()) {}
-  char_m(const char_m<Len> &s) : m_data(s.data()) {}
-  char_m(char_m<Len> &&s) : m_data(std::move(s.m_data)) {}
-  char_m<Len> &operator=(const char_m<Len> &s) {
+  bin_m() : m_data(){};
+  bin_m(const char *s) : m_data(s){};
+  bin_m(const ::sql::SQLString &s) : m_data(s.c_str()) {}
+  bin_m(const bin_m<Len> &s) : m_data(s.data()) {}
+  bin_m(bin_m<Len> &&s) : m_data(std::move(s.m_data)) {}
+  bin_m<Len> &operator=(const bin_m<Len> &s) {
     if (&s == this) {
       return *this;
     }
     m_data = s.m_data;
     return *this;
   };
-
 
   const std::string &data() const { return m_data; }//data的作用是什么？这两行函数的作用是什么？
   std::string &data() { return m_data; }
@@ -55,7 +54,7 @@ protected:
 
 namespace internal {
 template <class T> struct dump_col_type_creation;
-template <uint8_t Len> struct dump_col_type_creation<char_m<Len>> {
+template <uint8_t Len> struct dump_col_type_creation<bin_m<Len>> {
   static void dump(std::stringstream &ss) { ss << " CHAR(" << Len << ") "; }
 };
 } // namespace internal
@@ -64,18 +63,18 @@ namespace sql {
 template <class STMT, class T> struct mysql_bind_setter;
 
 template <class STMT, uint8_t Len>
-struct mysql_bind_setter<STMT, ::ff::mysql::char_m<Len>> {
+struct mysql_bind_setter<STMT, ::ff::mysql::bin_m<Len>> {
   static void bind(STMT stmt, int index,
-                   const ::ff::mysql::char_m<Len> &value) {
+                   const ::ff::mysql::bin_m<Len> &value) {
     stmt->setString(index, value.data());
   }
 };
 
 template <class T> struct mysql_rs_getter;
-template <uint8_t Len> struct mysql_rs_getter<::ff::mysql::char_m<Len>> {
+template <uint8_t Len> struct mysql_rs_getter<::ff::mysql::bin_m<Len>> {
   template <typename RST>
-  static ::ff::mysql::char_m<Len> get(RST r, const std::string &name) {
-    return ::ff::mysql::char_m<Len>(r->getString(name));
+  static ::ff::mysql::bin_m<Len> get(RST r, const std::string &name) {
+    return ::ff::mysql::bin_m<Len>(r->getString(name));
   }
 };
 
@@ -86,18 +85,17 @@ template <uint8_t Len> struct mysql_rs_getter<::ff::mysql::char_m<Len>> {
 
 
 
-
-// for varchar
+// for varbinary
 namespace ff {
 namespace mysql {
-template <uint16_t Len> struct varchar_m {
+template <uint16_t Len> struct varbin_m {
 public:
-  varchar_m() : m_data(){};
-  varchar_m(const char *s) : m_data(s){};
-  varchar_m(const ::sql::SQLString &s) : m_data(s.c_str()) {}//c_str: return char*
-  varchar_m(const varchar_m<Len> &s) : m_data(s.data()) {}
-  varchar_m(varchar_m<Len> &&s) : m_data(std::move(s.m_data)) {}
-  varchar_m<Len> &operator=(const varchar_m<Len> &s) {
+  varbin_m() : m_data(){};
+  varbin_m(const char *s) : m_data(s){};
+  varbin_m(const ::sql::SQLString &s) : m_data(s.c_str()) {}//c_str: return char*
+  varbin_m(const varbin_m<Len> &s) : m_data(s.data()) {}
+  varbin_m(varbin_m<Len> &&s) : m_data(std::move(s.m_data)) {}
+  varbin_m<Len> &operator=(const varbin_m<Len> &s) {
     if (&s == this) {
       return *this;
     }
@@ -114,7 +112,7 @@ protected:
 
 namespace internal {
 template <class T> struct dump_col_type_creation;
-template <uint16_t Len> struct dump_col_type_creation<varchar_m<Len>> {
+template <uint16_t Len> struct dump_col_type_creation<varbin_m<Len>> {
   static void dump(std::stringstream &ss) { ss << " VARCHAR(" << Len << ") "; }
 };
 } // namespace internal
@@ -123,18 +121,18 @@ namespace sql {
 template <class STMT, class T> struct mysql_bind_setter;
 
 template <class STMT, uint16_t Len>
-struct mysql_bind_setter<STMT, ::ff::mysql::varchar_m<Len>> {
+struct mysql_bind_setter<STMT, ::ff::mysql::varbin_m<Len>> {
   static void bind(STMT stmt, int index,
-                   const ::ff::mysql::varchar_m<Len> &value) {
+                   const ::ff::mysql::varbin_m<Len> &value) {
     stmt->setString(index, value.data());
   }
 };
 
 template <class T> struct mysql_rs_getter;
-template <uint16_t Len> struct mysql_rs_getter<::ff::mysql::varchar_m<Len>> {
+template <uint16_t Len> struct mysql_rs_getter<::ff::mysql::varbin_m<Len>> {
   template <typename RST>
-  static ::ff::mysql::varchar_m<Len> get(RST r, const std::string &name) {
-    return ::ff::mysql::varchar_m<Len>(r->getString(name));
+  static ::ff::mysql::varbin_m<Len> get(RST r, const std::string &name) {
+    return ::ff::mysql::varbin_m<Len>(r->getString(name));
   }
 };
 
@@ -145,17 +143,18 @@ template <uint16_t Len> struct mysql_rs_getter<::ff::mysql::varchar_m<Len>> {
 
 
 
-//for text_m
+// for Blob(using string method)
 namespace ff {
 namespace mysql {
-struct text_m {
+struct blob_m {
 public:
-  inline text_m() : m_data(){};
-  inline text_m(const char *s) : m_data(s){};
-  inline text_m(const ::sql::SQLString &s) : m_data(s.c_str()) {}
-  inline text_m(const text_m &s) : m_data(s.data()) {}
-  text_m(text_m &&s) : m_data(std::move(s.m_data)) {}
-  text_m  &operator=(const text_m &s) {
+  inline blob_m() : m_data(){};
+  inline blob_m(const char *s) : m_data(s){};
+  // inline blob_m(const std::istream *s) : ms_data(&s);
+  inline blob_m(const ::sql::SQLString &s) : m_data(s.c_str()) {}
+  inline blob_m(const blob_m &s) : m_data(s.data()) {}
+  inline blob_m(blob_m &&s) : m_data(std::move(s.m_data)) {}
+  blob_m &operator=(const blob_m &s) {
     if (&s == this) {
       return *this;
     }
@@ -168,11 +167,12 @@ public:
 
 protected:
   std::string m_data;
+  // std::istream ms_data;
 };
 
 // namespace internal {
 // template <class T> struct dump_col_type_creation;
-// template <uint16_t Len> struct dump_col_type_creation<text_m> {
+// template <uint16_t Len> struct dump_col_type_creation<blob_m> {
 //   static void dump(std::stringstream &ss) { ss << "TEXT(" << Len << ") "; }
 // };
 // } // namespace internal
@@ -181,18 +181,18 @@ namespace sql {
 template <class STMT, class T> struct mysql_bind_setter;
 
 template <class STMT>
-struct mysql_bind_setter<STMT, ::ff::mysql::text_m > {
+struct mysql_bind_setter<STMT, ::ff::mysql::blob_m > {
   static void bind(STMT stmt, int index,
-                   const ::ff::mysql::text_m  &value) {
+                   const ::ff::mysql::blob_m  &value) {
     stmt->setString(index, value.data());
   }
 };
 
 template <class T> struct mysql_rs_getter;
-template <> struct mysql_rs_getter<::ff::mysql::text_m>{
+template <> struct mysql_rs_getter<::ff::mysql::blob_m>{
   template <typename RST>
-  static ::ff::mysql::text_m get(RST r, const std::string &name) {
-    return ::ff::mysql::text_m (r->getString(name));
+  static ::ff::mysql::blob_m get(RST r, const std::string &name) {
+    return ::ff::mysql::blob_m (r->getString(name));
   }
 };
 
